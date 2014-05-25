9325e654cfda0aab9cf5372f9abe37dc77eada12
Piers Rudgard-Redsell <piersrr@gmail.com>
2014-05-25 14:04:08 +0100

    Initial Commit

---
+++ MatchedUp/CCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CCAppDelegate.h
+//  MatchedUp
+//
+//  Created by Piers Rudgard-Redsell on 25/05/2014.
+//  Copyright (c) 2014 Airbyte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MatchedUp/CCFirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCFirstViewController.h
+//  MatchedUp
+//
+//  Created by Piers Rudgard-Redsell on 25/05/2014.
+//  Copyright (c) 2014 Airbyte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCFirstViewController : UIViewController
+
+@end
